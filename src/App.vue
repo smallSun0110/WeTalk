274273e9c8ebbb45b744f5a2bbf2eead08fcee68
smallSun <1248010874@qq.com>
2024-08-27 16:11:44 +0800

    feat:first commit

---
+++ src/App.vue
@@ -0,0 +1,11 @@
+<script setup></script>
+
+<template>
+  <div>wetalk</div>
+</template>
+
+
+<style lang="less">
+
+
+</style>
---
+++ vite.config.js
@@ -0,0 +1,8 @@
+import UnoCSS from 'unocss/vite'
+import {defineConfig} from 'vite'
+import vue from '@vitejs/plugin-vue'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+    plugins: [vue(), UnoCSS(),],
+})
